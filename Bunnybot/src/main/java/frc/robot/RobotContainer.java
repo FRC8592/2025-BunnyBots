d9b75823c675868e6bc965cd0de7222aa43601ed
Zaid Rasti <zaid.rski@gmail.com>
2025-11-02 19:41:24 -0500

    Created a binding for the launcher to launch the ball while the right trigger is held

--- Bunnybot/src/main/java/frc/robot/RobotContainer.java
+++ Bunnybot/src/main/java/frc/robot/RobotContainer.java
@@ -4,14 +4,19 @@
 
 package frc.robot;
 
-// import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
+import java.util.Set;
+
 // import frc.robot.commands.ExampleCommand;
 // import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.CONTROLLERS;
+import frc.robot.subsystems.TestingLauncher1;
+import frc.robot.subsystems.TestingLauncher2;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,12 +25,18 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  private static final CommandXboxController driverController = new CommandXboxController(
+      CONTROLLERS.DRIVER_PORT
+  );
 
+  private final TestingLauncher1 testingLauncher1;
+  // The robot's subsystems and commands are defined here...
+  private final Trigger LAUNCH = driverController.rightTrigger();
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    testingLauncher1 = new TestingLauncher1();
     // Configure the trigger bindings
     configureBindings();
   }
@@ -40,6 +51,7 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    LAUNCH.whileTrue(new DeferredCommand(() -> testingLauncher1.setLauncherCommand(0.8), Set.of(testingLauncher1))).onFalse(testingLauncher1.stopLauncherCommand());
   
   }
 
