f0d86bf483e3c77c80ba52c676f6e921522ea253
Joe Thompson <jgthomps@gmail.com>
2025-11-25 20:19:15 -0500

    Updated camera position for new mount

--- Bunnybot/src/main/java/frc/robot/Constants.java
+++ Bunnybot/src/main/java/frc/robot/Constants.java
@@ -34,8 +34,7 @@ public final class Constants {
 
     public final class VISION {
         public static final String LOG_PATH = SHARED.LOG_FOLDER+"Vision";
-        public static final Transform3d CAMERA_OFFSETS = (
-            new Transform3d(new Translation3d(0.304, 0.008, 0.127), new Rotation3d(0, Math.toRadians(45), 0)));
+        public static final Transform3d CAMERA_OFFSETS = new Transform3d(new Translation3d(0.275, -0.095, 0.255), (new Rotation3d(0.0, Math.toRadians(-48.0), 0.0)));
         public static final String CAMERA_NAME = (
             "Arducam_OV9782_B" 
         );
