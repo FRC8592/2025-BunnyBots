3f1cd920ffd2a780eaf6b164fba80cd325134b33
RohanAgarwal114 <rohan.agarwal234@gmail.com>
2025-12-02 20:05:58 -0500

    added sixlumens basic autocommands

--- Bunnybot/src/main/java/frc/robot/commands/autonomous/autos/SixLumens.java
+++ Bunnybot/src/main/java/frc/robot/commands/autonomous/autos/SixLumens.java
@@ -5,13 +5,31 @@ import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.Suppliers;
 import frc.robot.commands.autonomous.AutoCommand;
 import frc.robot.commands.largecommands.*;
+import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Indexer;
 
 
 public class SixLumens extends AutoCommand {
-   public SixLumens(String color){
+   public SixLumens(String color, Launcher launcher, Indexer indexer){
       super(
-        
-       new FollowPathCommand(getChoreoTrajectory("CollectFirst"), Suppliers.isRedAlliance, ""),
+        new FollowPathCommand(getChoreoTrajectory("Shoot"), Suppliers.isRedAlliance, "")
+        .andThen(launcher.setLauncherCommand(
+         0.44, 0.30   // bottom %, top %
+     ).withTimeout(2.0))  // spin up time (tune this)
+     .andThen(
+         launcher
+             .setLauncherCommand(0.44, 0.30)   // keep launcher running
+             .alongWith(
+                 // feed with indexer 
+                 indexer.setMotorPercentOutputCommand(1)
+             )
+             .withTimeout(2.0)                 // feed time (tune this)
+     )
+     // 4) Stop launcher after feeding
+     .andThen(
+         launcher.stopLauncherCommand()
+     ).andThen(
+       new FollowPathCommand(getChoreoTrajectory("CollectFirst"), Suppliers.isRedAlliance, "")),
        /*
         * .andThen(Intake Command until Indexer detects 1 Lumen in its system))
         * .andThen(Indexer Command until their conditional is satisfied)
